13cee919a1c252f7f591c511ccd10ff21201d938
davu111 <vuvanvuonglqm@gmail.com>
2026-05-11 10:48:40 +0700

    [UPDATE] employee service

--- backend/employee-service/src/main/java/com/ecotel/employee_service/enums/AccessDirection.java
+++
@@ -1,7 +0,0 @@
-package com.ecotel.employee_service.enums;
-
-// Enum cho hướng ra vào
-public enum AccessDirection {
-    IN,  // Vào kho
-    OUT  // Ra kho
-}
--- backend/employee-service/src/main/java/com/ecotel/employee_service/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecotel.employee_service.repository;
-
-import com.ecotel.employee_service.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, String> {
-}
